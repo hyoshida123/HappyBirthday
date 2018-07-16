b4fd2b080c4e9800fd2aef796d3870edb204dfa2
hyoshida <hyoshida@git.com>
2018-07-15 17:15:56 -0700

    Initial commit

---
+++ app/src/androidTest/java/io/github/hyoshida123/happybirthday/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package io.github.hyoshida123.happybirthday
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("io.github.hyoshida123.happybirthday", appContext.packageName)
+    }
+}
